24050f2dc8dad6176672b140e699683e126cd71a
yshydv77 <yshydv77@gmail.com>
2025-08-13 16:30:02 +0530

    Time: 0 ms (100%), Space: 8.2 MB (88.44%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        string result= "";
+        unordered_map<char,int>mp;
+        for(int i = 0 ; i < s.size() ; i++){
+            mp[s[i]]++;
+        }
+        for(int i = 0 ; i < order.size() ; i++){
+            char ch = order[i];
+            auto findChInMp = mp.find(ch);
+            if(findChInMp != mp.end()){
+                // then the element is present in the s string
+                auto k = findChInMp->second;
+                if( k > 1){
+                   while(k--){
+                    result.push_back(findChInMp->first);
+                   }
+                }
+                else{
+                    result.push_back(findChInMp->first);
+                }
+            }
+
+        }
+        for(int i = 0 ; i < s.size() ; i++){
+            char ch = s[i];
+            int findChInResult = result.find(ch);
+            if(findChInResult  == -1){
+                auto k = mp[ch];
+                while(k--){
+                    result.push_back(ch);
+                }
+            }
+        }
+        return result;
+    }
+};
