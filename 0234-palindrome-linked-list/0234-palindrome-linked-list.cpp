658cd6720104ba1ae173d0d3d0aaaab751e1b09c
yshydv77 <yshydv77@gmail.com>
2025-09-02 20:51:05 +0530

    Time: 0 ms (100%), Space: 118.1 MB (71.21%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -2,7 +2,7 @@
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
- *     ListNode *next;
+ *     ListNode* next;
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
@@ -13,26 +13,27 @@ public:
     int getLength(ListNode* head){
         ListNode* temp = head;
         int count = 0;
-        while(temp!= NULL){
+        while(temp != NULL){
             count++;
             temp = temp->next;
         }
         return count;
     }
     ListNode* mid(ListNode* head){
-        ListNode* temp = head;
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
         ListNode* slow = head;
         ListNode* fast = head;
         while(fast != NULL && fast->next != NULL){
-            if(slow != temp){
-                temp = temp->next;
-            }
             slow = slow->next;
             fast = fast->next->next;
         }
-        return temp;
+
+        return slow;
     }
-    ListNode* reverseLL( ListNode* current){
+    ListNode* reverse(ListNode* head){
+        ListNode* current = head;
         ListNode* previous = NULL;
         while(current != NULL){
             ListNode* forward = current->next;
@@ -42,54 +43,28 @@ public:
         }
         return previous;
     }
-    void printLL(ListNode* head){
-        ListNode* temp = head;
-        while(temp!=NULL){
-            cout<<temp->val<<" ";
-            temp = temp->next;
-        }
-        cout<<endl;
-    }
     bool isPalindrome(ListNode* head) {
-        if(head->next == NULL){
+        if(head == NULL || head->next == NULL){
             return true;
         }
-        ListNode* slow = mid(head);
-        
-        ListNode* mid = slow->next;
-       
-        // cout<<"Printing the linked list before reversal "<<endl;
-        // printLL(head);
-        int length = getLength(head);
-        ListNode* previous = NULL;
-        if(length & 1 ){
-            previous = reverseLL(mid->next);
-        }
-        else{
-            previous = reverseLL(mid);
-        }
-        slow->next = previous;
 
-        // cout<<"Printing the linked list after reversal "<<endl;
-        printLL(head);
-
+        int len = getLength(head);
+        ListNode* m = mid(head);
+        if(len & 1){// odd length ki linked list hai 
+            m = m->next;
+        }
         ListNode* temp1 = head;
-        // cout<<"value of temp1 is : "<<temp1->val<<endl;
+        ListNode* temp2 = reverse(m);
 
-        ListNode* temp2 = previous;
-        // cout<<"value of temp2 is : "<<temp2->val<<endl;
-
-        while(temp2!=NULL){
-            if(temp1->val != temp2->val){
+        while(temp2 != NULL){
+            if(temp1->val !=  temp2->val){
                 return false;
             }
             temp1 = temp1->next;
             temp2 = temp2->next;
-
         }
 
         return true;
-
 
     }
 };
