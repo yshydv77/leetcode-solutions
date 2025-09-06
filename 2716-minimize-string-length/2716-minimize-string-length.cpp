64c98af2479ebd21bda7e424d1209b73fe961505
yshydv77 <yshydv77@gmail.com>
2025-09-07 02:38:34 +0530

    Time: 16 ms (57.34%), Space: 12 MB (84.43%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.cpp
+++ 2716-minimize-string-length/2716-minimize-string-length.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     int minimizedStringLength(string s) {
-        set<char>st;
-        st.insert(s.begin() , s.end());
-        return st.size();
+       sort(s.begin() , s.end());
+       string ans = "";
+       for(int i = 0 ; i < s.size() ; i++){
+        if(ans.empty() ||ans.back() != s[i]){
+            ans.push_back(s[i]);
+        }
+        else{
+            continue;
+        }
+       }
+    return ans.size();
+    
     }
 };
