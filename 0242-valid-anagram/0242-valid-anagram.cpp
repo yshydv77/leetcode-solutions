ac47c76383eade8a6006e46400b76166b0b8163e
yshydv77 <yshydv77@gmail.com>
2025-09-07 20:15:29 +0530

    Time: 0 ms (100%), Space: 9.7 MB (48.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(t.size() > s.size()) return false;
         vector<int>visited(26,0);
         for(auto it : s){
             visited[it-'a']++;
