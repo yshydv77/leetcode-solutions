4e88de10db31ce5a47c4345086d9775a654b64eb
yshydv77 <yshydv77@gmail.com>
2025-10-16 20:00:54 +0530

    Time: 0 ms (100%), Space: 23.2 MB (94.33%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,33 +1,49 @@
 class MinStack {
 public:
-    vector<int>st;
+    vector<pair<int,int>>st;
     MinStack() {
         
     }
     
     void push(int val) {
-        st.push_back(val);
+        pair<int,int>p;
+        p.first = val;
+        if(st.empty()){
+            p.second = val;
+        }
+        else{
+            if(st.back().second > val){
+                // matlab mere minimum element change ho gaya hai and new minimum element mera ye value hoga
+                p.second = val;
+            }
+            else{
+                p.second = st.back().second;
+            }
+        }
+
+        st.push_back(p);
     }
     
     void pop() {
-        if(st.size() != 0){
-            st.pop_back();   
+        if(st.empty()){
+           
         }
+        st.pop_back();
     }
     
     int top() {
-        if(st.size()){
-            return st.back();
+        if(st.empty()){
+            return -1;
         }
-        return -1;
+        return st.back().first;
     }
     
     int getMin() {
-        if(st.size()){
-            return *(min_element(st.begin() , st.end()));
+        if(st.empty()){
+            return -1;
         }
 
-        return -1;
+        return st.back().second;
     }
 };
 
