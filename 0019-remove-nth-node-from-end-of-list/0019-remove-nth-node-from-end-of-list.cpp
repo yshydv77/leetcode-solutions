494487fa0e0b5bf7528c13bbdeda976f356de440
yshydv77 <yshydv77@gmail.com>
2025-11-26 10:52:50 +0530

    Time: 0 ms (100%), Space: 14.8 MB (91.36%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,32 +11,93 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head->next == 0 && n == 1){return 0;}
 
-        int len = 0;
-        auto it = head;
-        while(it){
-            len++;
-            it = it->next;
-        }
+      if( head->next == NULL && n == 1){
+        return NULL;
+      }
 
-        auto pos = len - n;
-        it = head;
-        for(int i = 0 ; i < pos-1 ; i++){
-            it = it->next;
+      auto it  = head;
+      int len = 0 ;
+      while(it != NULL){
+        len++;
+        it = it->next;
+      }
 
-        }
-        if(pos == 0){
-            head = head->next;
-            it->next = 0;
-            delete it;
-            return head;
-        }
-        auto current = (it->next);
-        auto forward = current->next;
-        it->next = forward;
-        current->next = 0;
-        delete(current);
+      it = head;
+      int position = len - n ;
+      for(int i = 0 ; i < position - 1 ; i++){
+        it = it->next;
+      }
+     
+      if(position == 0){
+        auto temp = it;
+        it = it->next;
+        head = it;
+        temp->next = 0;
+        delete(temp);
         return head;
+      }
+      
+      auto temp = it->next;
+      it->next = temp->next;
+      temp->next = NULL;
+      delete(temp);
+      return head;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // if(head->next == 0 && n == 1){return 0;}
+
+        // int len = 0;
+        // auto it = head;
+        // while(it){
+        //     len++;
+        //     it = it->next;
+        // }
+
+        // auto pos = len - n;
+        // it = head;
+        // for(int i = 0 ; i < pos-1 ; i++){
+        //     it = it->next;
+
+        // }
+        // if(pos == 0){
+        //     head = head->next;
+        //     it->next = 0;
+        //     delete it;
+        //     return head;
+        // }
+        // auto current = (it->next);
+        // auto forward = current->next;
+        // it->next = forward;
+        // current->next = 0;
+        // delete(current);
+        // return head;
     }
 };
