18c44e6c55aaaa06a98e5886ab4dfb7bd32205ff
yshydv77 <yshydv77@gmail.com>
2026-03-16 08:33:59 +0530

    Time: 3 ms (9.78%), Space: 8.9 MB (67.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
-    bool check(char current , char top){
-        if((top == '(' && current == ')') || (top == '{' && current == '}') || (top == '[' && current == ']')){
-            return true;
-        }
+    bool check_brackets(char s , char e){
+      if((s == '(' && e == ')') || (s=='{' && e=='}') || (s=='[' && e==']'))
+        return true;
+      else 
         return false;
     }
     bool isValid(string s) {
-        
-        if(s.size() == 0){
-            return true;
-        }
-
         stack<char>st;
-        for(int i = 0 ; i < s.size(); i++){
-            char ch = s[i];
-            if(ch == '(' || ch == '{' || ch == '['){
-                st.push(ch);
-            }
-            else{
-                if(st.empty()){
-                    return false;
-                }
-                bool ans = check(ch ,  st.top());
-                if(!ans){// condition for mismatch between the parenthesis 
-                    return false;
-                }
-                st.pop();
-            }
+        for (auto it : s){
+       
+          if(it == '(' || it == '{' || it == '[')
+            st.push(it);
+          else{
+            if(!st.empty() && check_brackets(st.top() , it))
+              st.pop();
+            else
+              return false;
+          }
         }
 
-        if(!st.empty()){
-            return false;
-        }
-        return true;
+        return st.empty();
     }
 };
