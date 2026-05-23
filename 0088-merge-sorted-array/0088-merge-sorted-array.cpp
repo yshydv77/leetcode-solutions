fb43022b2de656bc2d9d5274feb068d616ca7377
yshydv77 <yshydv77@gmail.com>
2026-05-23 15:32:44 +0530

    Time: 0 ms (100%), Space: 12.4 MB (40.25%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,105 +1,38 @@
 class Solution {
 public:
-    void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
+    void merge(vector<int>& a, int m, vector<int>& b, int n) {
+      int i = 0 ;
+      int j = 0;
 
-       
-        int i = m-1,
-            j = n-1, 
-            k = n1.size()-1;
-
-        
-        while(i >=0 && j >= 0){
-            if(n1[i] > n2[j]){
-                n1[k] = n1[i];
-                i--;
-                k--;
-            }
-            else if(n1[i] == n2[j]){
-                n1[k] = n2[j];
-                j--;
-                k--;
-            }
-            else{
-                n1[k] = n2[j];
-                j--;
-                k--;
-            }
+      vector<int>ans;
+      while(i< m && j < n){
+        if(a[i] < b[j]){
+          ans.push_back(a[i]);
+          i++;
         }
-
-
-
-        while(i >= 0){
-            n1[k] = n1[i];
-            k-- ;
-            i--; 
+        else if(b[j] < a[i]){
+          ans.push_back(b[j]);
+          j++;
         }
-
-        while(j >= 0){
-            n1[k] = n2[j];
-            j--;
-            k--;
+        else{
+          ans.push_back(a[i]);
+          i++;
+          ans.push_back(b[j]);
+          j++;
         }
+      }
 
+      while(i<m){
+        ans.push_back(a[i]);
+        i++;
+      }
+      while(j<n){
+        ans.push_back(b[j]);
+        j++;
+      }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // vector<int>ans;
-        // int i1 = 0;
-        // int i2 = 0;
-        // while(i1 < m && i2 < n){
-        //     if(nums1[i1] < nums2[i2]){
-        //         ans.push_back(nums1[i1]);
-        //         i1++;
-        //     }
-        //     else if(nums1[i1] > nums2[i2]){
-        //         ans.push_back(nums2[i2]);
-        //         i2++;
-        //     }
-        //     else{
-        //         // if both the elements are = 
-        //         ans.push_back(nums1[i1]);
-        //         ans.push_back(nums2[i2]);
-        //         i1++;
-        //         i2++;
-        //     }
-        // }
-
-        // while(i1<m){
-        //     ans.push_back(nums1[i1]);i1++;
-        // }
-        // while(i2 < n){
-        //     ans.push_back(nums2[i2]);i2++;
-        // }
-
-        // for(int i = 0 ; i < ans.size() ; i++){
-        //     nums1[i] = ans[i];
-        // }
-
-
+      for(int i = 0 ; i < m+n ; i++){
+        a[i] = ans[i];
+      }
     }
 };
