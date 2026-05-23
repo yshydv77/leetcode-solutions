beca2ddf04d6911c3f585e3fe6e4baa63ac75c2e
yshydv77 <yshydv77@gmail.com>
2026-05-23 15:21:47 +0530

    Time: 59 ms (19.92%), Space: 35.1 MB (11.76%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,46 +1,44 @@
 class Solution {
 public:
-    void twoSumHelper(int f , vector<int>&n , vector<vector<int>>&ans){
-        int i = f+1;
-        int j  = n.size()-1;
-
-        while(i < j){
-            int sum = n[f] + n [i]  + n[j];
-            if(sum > 0 ){
-                j = j -1;
-            }else if(sum < 0 ){
-                i = i + 1;
-            }else{
-                ans.push_back({n[f] , n[i] , n[j]});
-                i++;
-                j--;
-
-                while(i < j && n[i] == n[i-1]){
-                    i++;
-                }
-                while(i<j && n[j] == n[j+1]){
-                    j--;
-                }
-            }
-        }
-    }
-    vector<vector<int>> threeSum(vector<int>& n) {
+    vector<vector<int>> threeSum(vector<int>& a) {
         vector<vector<int>>ans;
-        sort(n.begin() , n.end());
+        sort(a.begin() , a.end());
+        int n = a.size();
+        for(int i = 0 ; i < n-2 ; i++){
+          int left = i+1;
+          int right = n-1;
 
-        for(int f = 0 ; f < n.size() ; f++){
-            if(n[f] > 0 ){
-                break;
+          int target = (a[i] * (-1));
+          if(i>0 && a[i] == a[i-1]){
+            continue;
+          }
+          // two sum approach for each i 
+          while(left < right){
+            int sum = a[left] + a[right];
+            if(sum  < target){
+              left++;
             }
-
-            if(f == 0 || n[f] != n[f-1]){
-                twoSumHelper(f , n , ans);
+            else if(sum > target){
+              right--;
             }
-                
+            else{
+              // sum == target
+              vector<int>an;
+              an.push_back(a[i]);
+              an.push_back(a[left] );
+              an.push_back(a[right]);
+              ans.push_back(an);
+              left++;
+              right--;
+              while(left < n && a[left] == a[left-1]){
+                left++;
+              }
+              while(right>=0 && a[right]==a[right+1]){
+                right--;
+              }
+            }
+          }
         }
-
-
         return ans;
-
     }
 };
