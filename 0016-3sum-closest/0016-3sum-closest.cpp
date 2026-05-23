a2c29f657a2ef13a556717d0ba53c508aebcba34
yshydv77 <yshydv77@gmail.com>
2026-05-23 16:25:14 +0530

    Time: 11 ms (60.56%), Space: 14.1 MB (33.41%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-    void twoSumHelper(int f , int &target ,int &closestSum, vector<int>&nums){
-        int i = f+1;
-        int j = nums.size()-1;
-
-        while(i < j){
-            int sum = nums[f] + nums[i] + nums[j];
-
-            int diff = abs (sum - target);
-
-            int bdiff = abs(closestSum - target);
-
-            if(diff < bdiff){
-                bdiff = diff;
-                closestSum = sum;
-            }
-
-            if(sum < target){
-                i++;
-            }else if(sum > target){
-                j--;
-            }else{
-                i++;
-                j--;
-                while(i<j && nums[i] == nums[i-1]){
-                    i++;
-                }
-                while(i<j && nums[j] == nums[j+1]){
-                    j--;
-                }
-            }
-
-        }
-    }
     int threeSumClosest(vector<int>& nums, int target) {
-        
-        sort(nums.begin() , nums.end());
-        int closestSum = nums[0] + nums[1] + nums[2];
-        for(int f = 0 ; f < nums.size() ; f++){
-            // two sum apply karva do 
-            twoSumHelper(f , target ,closestSum, nums);
-        }
+      int n = nums.size();
+      sort(nums.begin() , nums.end());
 
-        return closestSum;
+      int resultSum = 0 ;
+      int maxDiff = INT_MAX;
+
+      for(int i = 0 ; i < n-2 ;i++){
+        int left = i+1;
+        int right = n-1;
+
+        while(left < right){
+          int currentSum = nums[i] + nums[left] + nums[right];
+
+          if(currentSum == target){
+            return currentSum;// because here my diff will be minimum from the target which is going to be zero 
+          }
+          int diff = abs(target - currentSum);
+          if(diff < maxDiff){
+            maxDiff = diff;
+            resultSum = currentSum;
+          }
+          if(currentSum < target){
+            left++;
+          }
+          else{
+            right--;
+          }
+
+          
+        }
+      }
+
+      return resultSum;
     }
 };
