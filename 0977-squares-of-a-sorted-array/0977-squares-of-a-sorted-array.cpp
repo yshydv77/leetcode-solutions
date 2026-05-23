f258bf8f6e55f9b69234afa2589ad49a3660b284
yshydv77 <yshydv77@gmail.com>
2026-05-23 11:48:56 +0530

    Time: 0 ms (100%), Space: 30.3 MB (65.97%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -5,35 +5,46 @@ public:
         nums[0] *= nums[0];
         return nums;
       }
-      vector<int>ans;
+      vector<int>ans(nums.size());
       int i = 0;
       int j = nums.size()-1;
+      int k = nums.size()-1;
       while(i<=j){
         int sqi = nums[i] * nums[i];
         int sqj = nums[j] * nums[j];
 
         if(sqi < sqj){
-          ans.push_back(sqj);
+          ans[k] = sqj;
+          k--;
+          // ans.push_back(sqj);
           j--;
         }
         else if(sqi > sqj){
-          ans.push_back(sqi);
+          ans[k] = sqi;
+          k--;
+          // ans.push_back(sqi);
           i++;
         }
         else if(sqi == sqj && i != j ){
-          ans.push_back(sqi);
-          ans.push_back(sqj);
+          ans[k] = sqi;
+          k--;
+          ans[k] = sqj;
+          k--;
+          // ans.push_back(sqi);
+          // ans.push_back(sqj);
           i++;
           j--;
         }
         else{
-          ans.push_back(sqi);
+          ans[k] = sqi;
+          k--;
+          // ans.push_back(sqi);
           i++;
           j--;
         }
       }
-      reverse(ans.begin() , ans.end());
-      
+      // reverse(ans.begin() , ans.end());
+
       return ans;
     }
 };
