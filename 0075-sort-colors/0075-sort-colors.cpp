298f526ea560990acfbb13f921993121ab43fad3
yshydv77 <yshydv77@gmail.com>
2026-05-24 08:19:14 +0530

    Time: 0 ms (100%), Space: 11.7 MB (10.75%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,78 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-
-        
-
-      int start = 0 ;
-      int end = nums.size()-1;
-      int mid = 0;
-
-      while(mid <= end){
-        if(nums[mid] == 2){
-          swap(nums[mid] , nums[end]);
-          end--;
+      int low = 0;
+      int mid = 0 ;
+      int high = nums.size()-1;
+      while(mid<=high){
+        if(nums[mid] == 0){
+          swap(nums[mid] , nums[low]);
+          low++;
+          mid++;
         }
         else if(nums[mid] == 1){
-          swap(nums[mid] , nums[start]);
           mid++;
         }
         else{
-          swap(nums[mid] , nums[start]);
-          start++;
-          mid++;
+          // nums[mid] == 2
+          swap(nums[mid] , nums[high]);
+          high--;
         }
       }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // int low = 0 ;
-        // int mid = 0;
-        // int high = nums.size()-1;
-        // while(mid <= high){
-        //     if(nums[mid] == 0){
-        //         // zero ke liye low wala hai 
-        //         // toh uske sath mein swap lkarva do 
-        //         swap(nums[low] , nums[mid]);
-        //         mid++;low++;
-        //     }
-        //     else if(nums[mid] == 1){
-        //         mid++;
-        //         // 1 ke liye mid hi hai vahi current element ko track kar raha hai toh 
-        //     }
-        //     else{
-        //         swap(nums[mid] , nums[high]);
-        //         high--;
-        //     }
-        // }
-        
     }
 };
