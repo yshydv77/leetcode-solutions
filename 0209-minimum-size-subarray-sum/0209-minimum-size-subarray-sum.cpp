b993fba83cc5be7f0d18be9e7a6bd8b7c939662e
yshydv77 <yshydv77@gmail.com>
2026-05-27 15:56:56 +0530

    Time: 3 ms (11.9%), Space: 32 MB (72.99%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-      int min_len = INT_MAX;
-      int sum  = 0 ;
+    int minSubArrayLen(int target, vector<int> &nums)
+{
+    // array subarray min length subarray whose sum>= target
+    int low = 0;
+    int csum = 0;
+    int min_len = INT_MAX;
+    for(int high = 0 ; high < nums.size() ; high++){
+        csum += nums[high];
+        while(csum >= target){
+            int len = high-low+1;
+            min_len = min(min_len , len);
 
-      // varialble size window hai isliye hum iske low and high ko zero se start karneg
-
-      // hiring -> hiring mein first hire karlo and then next student par socho ki hire karna hai yaa  phir nahi iske liye hi hum sum mein value add karlo and then high ko plus plus kardo 
-      // firing -> firing mein sum mein se value ghata do and low ko plus plus kardo 
-      int low = 0 ;
-      int high = 0;
-
-      while(high < nums.size()){
-        sum += nums[high];
-        while(sum >= target){
-          int len  = (high - low  + 1);
-          min_len = min (min_len , len);
-
-          sum -= nums[low];
-          low++;
+            csum -=nums[low ];
+            low++;
         }
-        high++;
-      }
-      
-
-    return (min_len == INT_MAX) ? 0 : min_len;
     }
+
+    return (min_len == INT_MAX ? 0 : min_len) ;
+}
 };
