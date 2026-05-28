9a6e612da430ee11f7f28d548dfae767c2106e62
yshydv77 <yshydv77@gmail.com>
2026-05-28 12:20:04 +0530

    Time: 14 ms (39.84%), Space: 12.5 MB (38.64%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,13 +5,11 @@ public:
     // string substring longest -> sliding window ka question ho sakta hai 
     // sliding window se sochna strat karte hai 
     // variable sizd window hai 
-    if(s.size() == 0){
-        return 0;
-    }
+   
     int low = 0;
     int high =  0;
 
-    int maxLen = 1;
+    int maxLen = 0;
     unordered_map<char,int>freq;
     for(high = 0 ; high < s.size() ; high++){
         freq[s[high]]++;
