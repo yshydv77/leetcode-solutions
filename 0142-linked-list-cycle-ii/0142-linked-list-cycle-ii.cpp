e7c2cb43ee76d8ce57ed701503f87779424c5c6d
yshydv77 <yshydv77@gmail.com>
2026-05-29 15:56:29 +0530

    Time: 10 ms (46.35%), Space: 11.4 MB (53.59%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,29 +8,29 @@
  */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
-        if(head == NULL || head->next == NULL){
-            return NULL;
-        }
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        // detect a loop wali method use karlo to find where slow and fast is equal
-        while(fast != NULL){
-            fast = fast->next;
-            if(fast != NULL){
-                fast = fast->next;
+    
+ListNode *detectCycle(ListNode *head)
+{
+    // return the starting point of the cycle
+    // ll starting point of the cycle 
+    // slow and fast pointer ka question ha 
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while(fast!= NULL && fast->next!=NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            // meeting point par mil gaye hai 
+            slow = head;
+            while(slow != fast){
                 slow = slow->next;
-                if(slow == fast){
-                    break;
-                }
+                fast = fast->next;
             }
+
+            return slow;
         }
-        fast = head;
-        while(fast != slow && fast != NULL && slow != NULL){
-            fast = fast->next;
-            slow = slow->next;
-        }
-        return slow;
     }
+    return NULL;
+}
 };
