db3d13e81c6bd59d26bba2c0827d5c1bd742fc36
yshydv77 <yshydv77@gmail.com>
2026-05-29 14:01:03 +0530

    Time: 19 ms (74.86%), Space: 139.1 MB (89.11%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,53 +2,44 @@ class Solution {
 public:
 vector<int> maxSlidingWindow(vector<int> &nums, int k)
 {
-    if (k == 1)
+    if(k == 1){
         return nums;
-
-    int low = 0;
-    int high = k - 1;
-
-    vector<int> ans;
-    deque<int> dq; // stores indices
-
-    // Build first window
-    for (int i = 0; i <= high; i++)
-    {
-        while (!dq.empty() && nums[dq.back()] <= nums[i])
-        {
-            dq.pop_back();
-        }
-
-        dq.push_back(i);
     }
 
+    int low = 0;
+    int high = k-1;
+    deque<int>dq;
+    vector<int>ans;
+    for(int i = 0 ; i <= high ; i++){
+        while(!dq.empty() && nums[dq.back()] <= nums[i]){
+            dq.pop_back();
+        }
+        dq.push_back(i);
+    }
     ans.push_back(nums[dq.front()]);
 
-    while (high < nums.size())
-    {
+    while(high < nums.size()){
         low++;
         high++;
-
-        if (high == nums.size())
+        if(high == nums.size()){
             break;
+        }   
 
-        // Remove indices that are outside the window
-        while (!dq.empty() && dq.front() < low)
-        {
+        while(!dq.empty() && dq.front() < low ){
             dq.pop_front();
         }
 
-        // Insert new element while maintaining decreasing order
-        while (!dq.empty() && nums[dq.back()] <= nums[high])
-        {
+        while(!dq.empty() && nums[dq.back()] <= nums[high]){
             dq.pop_back();
         }
 
         dq.push_back(high);
-
         ans.push_back(nums[dq.front()]);
     }
 
+
     return ans;
+
+    
 }
 };
