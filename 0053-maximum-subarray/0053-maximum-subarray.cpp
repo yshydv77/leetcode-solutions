cd7b48d613b326d344e9dae5853400b217a631c5
yshydv77 <yshydv77@gmail.com>
2026-05-31 14:54:46 +0530

    Time: 0 ms (100%), Space: 71.9 MB (15.13%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -6,16 +6,17 @@ public:
     // all int 
     // kadaens ka question hai 
     // sliding wndow fail here because negative elements are also here
-    int bestending = nums[0];
+    int prevEnding = nums[0];
     int ans = nums[0];
     for(int i = 1 ; i < nums.size() ; i++){
         // bestending ke liye do option hai 
         // opt1 -> prvious ke sath mil jao 
         //opt2 -> apna start karo i ko hi lo and i se start karo
-        int opt1 = bestending + nums[i];
+        int opt1 = prevEnding + nums[i];
         int opt2 = nums[i];
-        bestending = max(opt1 , opt2 );
-        ans = max(ans , bestending);
+        int newEnding = max(opt1 , opt2);
+        ans = max(ans , newEnding);
+        prevEnding = newEnding;
     }
 
     return ans;
