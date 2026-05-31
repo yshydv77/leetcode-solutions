923697cb511482c9e4a2672614856aa44b731b9d
yshydv77 <yshydv77@gmail.com>
2026-05-31 20:23:11 +0530

    Time: 3 ms (14.47%), Space: 17.8 MB (58.96%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-//maximum product subarray
-int maxProduct(vector<int> &nums)
-{
-    if(nums.size() == 1){
-        return nums[0];
-    }
-    int maxending = nums[0];
-    int ans = nums[0];
-    int minending = nums[0];
-    for(int i =1 ;i<nums.size();i++){
+    int maxProduct(vector<int>& nums) {
+        int prevMaxEnding = nums[0];
+        int prevMinEnding = nums[0];
+        int ans = nums[0];
 
-        int opt1 = nums[i];
-        int opt2 = nums[i] * minending;
-        int opt3 = nums[i] * maxending;
-        maxending = max(opt1 , max(opt2,opt3));
-        minending = min(opt1, min(opt2, opt3));
-        ans = max(ans , max(maxending,minending));
-   
-    }
+        for(int i = 1 ; i < nums.size() ; i++){
+            int newMaxEnding = max({nums[i] , prevMaxEnding * nums[i] , prevMinEnding * nums[i]});
+            int newMinEnding = min({nums[i] , prevMaxEnding * nums[i] , prevMinEnding * nums[i]});
 
-    return ans;
-}
+            ans = max({ans , newMaxEnding , newMinEnding});
+            prevMinEnding = newMinEnding;
+            prevMaxEnding = newMaxEnding;
+        }
+
+        return ans;
+    }
 };
