89dea8aa8e0f068e3ec713b973c4db80b3376858
yshydv77 <yshydv77@gmail.com>
2026-06-02 15:38:47 +0530

    Time: 4 ms (33.9%), Space: 9.7 MB (48.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,17 +5,16 @@ public:
             return false;
         }
         vector<int>s_hashmap(26);
-        vector<int>t_hashmap(26);
         for(int i = 0 ; i < s.size() ; i++){
             s_hashmap[s[i]-'a']++;
         }
 
         for(int i = 0 ; i < t.size() ; i++){
-            t_hashmap[t[i]-'a']++;
+            s_hashmap[t[i]-'a']--;
         }
 
         for(int i = 0 ; i < 26;i++){
-            if(s_hashmap[i] != t_hashmap[i]){
+            if(s_hashmap[i] != 0){
                 return false;
             }
         }
