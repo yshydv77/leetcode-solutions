94d12d1ad0697b207e46489e5f3a3f9e0b128dcd
yshydv77 <yshydv77@gmail.com>
2026-06-03 12:40:21 +0530

    Time: 0 ms (100%), Space: 24.7 MB (12.88%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
-        int n = nums.size();
+        // array sort karne se faayada hoga and count karna hai two sum whose sum is lees than target
+        // two pointer pattern ka qustion hai 
+        sort(nums.begin(), nums.end());
         int count = 0 ;
-
-        for(int i = 0 ; i < n ; i++){
-            for ( int j = i+1 ; j < n ; j++){
-                if(nums[i] + nums[j] < target){
-                    count++;
-                }
+        int low  = 0 ;
+        int high = nums.size()-1;
+        while(low<high){
+            int sum = nums[low]+nums[high];
+            if(sum<target){
+                count+=(high-low);
+                low++;
             }
+            else if(sum >= target){
+                high--;
+            }
+           
         }
-
         return count;
-
     }
 };
