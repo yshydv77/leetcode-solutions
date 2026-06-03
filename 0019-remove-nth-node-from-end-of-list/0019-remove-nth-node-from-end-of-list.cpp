50864e0414ad8891402df8c1e7388594eb08e1b1
yshydv77 <yshydv77@gmail.com>
2026-06-03 14:00:58 +0530

    Time: 0 ms (100%), Space: 14.9 MB (92.3%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,45 +8,38 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head->next == nullptr && n == 1){
-            return nullptr;
-        }
-        int sz = 0 ;
-
-        ListNode* temp = head;
-        while(temp!=nullptr){
-            sz++;
-            temp=temp->next;
-        }
-        temp =  head;
-
-
-        int pos = sz - n;
-        // if(pos == 0){
-        //     ListNode* t = head;
-        //     head = head->next;
-        //     t->next = nullptr;
-        // }
-        for(int i = 0 ; i < pos-1; i++){
-            temp=temp->next;
-        }
-
-        if(temp->next->next == nullptr && pos != 0){
-            //last node ko delete karna tha
-            temp->next = nullptr;
-        }
-        else if(pos != 0) {
-            temp->next = temp->next->next;
-        }
-        else{
-            ListNode* t = head;
-            head = head->next;
-            t->next = nullptr;
-        }
-        return head;
+ListNode *removeNthFromEnd(ListNode *head, int n)
+{
+    if(head->next == nullptr && n == 1){
+        return nullptr;
     }
+    ListNode* slow = head;
+    ListNode* fast = head;
+    // create the gap between them
+    while(fast != nullptr && fast->next!=nullptr && n--){
+        fast = fast->next;
+    }
+
+    while(fast != nullptr && fast->next != nullptr){
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    if(n==1){
+        ListNode* temp = head;
+        head = head->next;
+        temp->next = nullptr;
+        delete temp;
+    }
+    else{
+    ListNode* temp = slow->next;
+    slow->next = temp->next;
+    temp->next = nullptr;
+    delete temp;
+    }
+    return head;
+
+}
 };
