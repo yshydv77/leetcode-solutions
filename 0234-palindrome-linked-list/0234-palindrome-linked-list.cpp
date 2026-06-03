d541dea20c1b4eaebee88a1e46a1ff94b17b7487
yshydv77 <yshydv77@gmail.com>
2026-06-03 15:54:08 +0530

    Time: 74 ms (5.04%), Space: 152.4 MB (5.09%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -2,7 +2,7 @@
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
- *     ListNode* next;
+ *     ListNode *next;
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
@@ -10,61 +10,32 @@
  */
 class Solution {
 public:
-    int getLength(ListNode* head){
-        ListNode* temp = head;
-        int count = 0;
-        while(temp != NULL){
-            count++;
-            temp = temp->next;
+bool isPalindrome(ListNode *head)
+{
+    ListNode* slow = head;
+    ListNode* fast = nullptr;
+    while(slow!= nullptr ){
+        ListNode* node = new ListNode(slow->val);
+        if(fast == nullptr){
+            fast = node;
         }
-        return count;
+        else{
+            node->next = fast;
+            fast = node;
+        }
+
+        slow = slow->next;
     }
-    ListNode* mid(ListNode* head){
-        if(head == NULL || head->next == NULL){
-            return head;
-        }
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast != NULL && fast->next != NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
 
-        return slow;
+    slow = head;
+    while(slow!= nullptr && fast!= nullptr){
+        if(slow->val != fast->val){
+            return false;
+        }
+        slow = slow->next;
+        fast = fast->next;
     }
-    ListNode* reverse(ListNode* head){
-        ListNode* current = head;
-        ListNode* previous = NULL;
-        while(current != NULL){
-            ListNode* forward = current->next;
-            current->next = previous;
-            previous = current;
-            current = forward;
-        }
-        return previous;
-    }
-    bool isPalindrome(ListNode* head) {
-        if(head == NULL || head->next == NULL){
-            return true;
-        }
+    return true;
 
-        int len = getLength(head);
-        ListNode* m = mid(head);
-        if(len & 1){// odd length ki linked list hai 
-            m = m->next;
-        }
-        ListNode* temp1 = head;
-        ListNode* temp2 = reverse(m);
-
-        while(temp2 != NULL){
-            if(temp1->val !=  temp2->val){
-                return false;
-            }
-            temp1 = temp1->next;
-            temp2 = temp2->next;
-        }
-
-        return true;
-
-    }
+}
 };
