e5fd1d04c94ce94a50f2d60ccb17d19be095465d
yshydv77 <yshydv77@gmail.com>
2026-06-03 17:24:30 +0530

    Time: 0 ms (100%), Space: 93.5 MB (30.65%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
-void print(vector<int>&p){
-    for(auto &it : p){
-        cout<<it<<" ";
-    }
-    cout<<endl;
-}
-vector<int> xorQueries(vector<int> &arr, vector<vector<int>> &queries)
-{
-    vector<int> ans;
-    vector<int>prefix(arr.size());
-    vector<int>suffix(arr.size());
-    int p = 0 ;
-    int s = 0;
-    for(int i = 0 ; i < arr.size() ; i++){
-        p ^= arr[i];
-        prefix[i] = p;
-    }
-
-    for(int i = arr.size()-1 ; i>= 0 ; i--){
-        s ^= arr[i];
-        suffix[i] = s ;
-
-    }
-
-    print(prefix);
-    print(suffix);
-    for(int i = 0 ; i < queries.size() ; i++){
-        int start = queries[i][0];
-        int end = queries[i][1];
-
-        if(start == 0){
-            ans.push_back(prefix[end]);
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int> ans;
+        vector<int> prefix(arr.size());
+        vector<int> suffix(arr.size());
+        int p = 0;
+        int s = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            p ^= arr[i];
+            prefix[i] = p;
         }
-        else{
-            int query_end = prefix[end];
-            int query_start = prefix[start-1];
-            int res = query_end ^ query_start;
-            ans.push_back(res);
-        }
-    }
 
-    return ans;
-}
+        for (int i = 0; i < queries.size(); i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+
+            if (start == 0) {
+                ans.push_back(prefix[end]);
+            } else {
+                int query_end = prefix[end];
+                int query_start = prefix[start - 1];
+                int res = query_end ^ query_start;
+                ans.push_back(res);
+            }
+        }
+
+        return ans;
+    }
 };
