614d5aa5a7c6f7e361dffb958c7de4f9fb4b64d7
yshydv77 <yshydv77@gmail.com>
2026-06-04 11:11:50 +0530

    Time: 0 ms (100%), Space: 13.5 MB (40.27%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -15,22 +15,40 @@ ListNode *reverseList(ListNode *head)
     if(head == nullptr || head->next == nullptr){
         return head;
     }
-    ListNode* slow = head;
-    ListNode* fast = nullptr;
-    while (slow!=nullptr)
-    {
-        ListNode* node = new ListNode(slow->val);
-        if(fast == nullptr){
-            fast = node;
-        }
-        else{
-            node->next = fast;
-            fast = node;
-        }
-        slow = slow->next;
+    
+    // optimal approach 
+    ListNode* prev_node = nullptr;
+    ListNode* curr_node = head;
+
+    while(curr_node != nullptr){
+        ListNode* next_node = curr_node->next;
+        curr_node->next = prev_node;
+
+        prev_node = curr_node;
+        curr_node = next_node;
     }
 
-    return fast;
+    return prev_node;
+
+
+    //approach my first time
+
+    // ListNode* slow = head;
+    // ListNode* fast = nullptr;
+    // while (slow!=nullptr && slow->next != nullptr)
+    // {
+    //     ListNode* node = new ListNode(slow->val);
+    //     if(fast == nullptr){
+    //         fast = node;
+    //     }
+    //     else{
+    //         node->next = fast;
+    //         fast = node;
+    //     }
+    //     slow = slow->next;
+    // }
+
+    // return fast;
     
 }
 };
