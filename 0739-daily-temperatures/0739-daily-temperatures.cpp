e82b15388a074d63ab1bf8fe21debdcdf40e305d
yshydv77 <yshydv77@gmail.com>
2026-06-06 16:23:44 +0530

    Time: 19 ms (71.31%), Space: 102.7 MB (92.28%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+vector<int> dailyTemperatures(vector<int>& temperatures) {
+    // mujhe isme most recenet element ki zarurat padh rahi hai isliye stack lagega
+    int n = temperatures.size();
+    stack<int>st;
+    vector<int>ans(n);
+    ans[n-1] = 0 ;
+    st.push(n-1);
+
+    for(int i = n-2; i >= 0 ; i -- ){
+        while(!st.empty() && temperatures[st.top()] <= temperatures[i]){
+            st.pop();
+        }
+
+        if(st.empty()){
+            ans[i] = 0 ;
+        }
+        else
+        {
+            ans[i] = st.top() - i;
+        }
+        
+        st.push(i);
+    }
+
+    return ans;
+
+}
+};
