729b2b92291fe7abed3aafa5dab84d0fae2f3c3c
yshydv77 <yshydv77@gmail.com>
2026-06-07 21:19:59 +0530

    Time: 3 ms (40.93%), Space: 12.4 MB (82.17%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    void nge(vector<int>&arr2 , vector<int>&next){
-        stack<int>st;
-        st.push(-1);
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+  int n = nums2.size();
+  vector<int> ans(n);
+  stack<int> st;
+  ans[n - 1] = -1;
+  st.push(nums2[n - 1]);
+  // finding next greater elements of nums2
 
-        for(int i = arr2.size()-1 ; i >= 0 ; i--){
-            int ele = arr2[i];
-            while(!st.empty() &&st.top() <= ele){
-                st.pop();
-            }
-
-            next[i] = (st.empty()) ? -1 :  st.top();
-            st.push(ele);
-        }
+  for (int i = n - 2; i >= 0; i--) {
+    while (!st.empty() && st.top() <= nums2[i]) {
+      st.pop();
     }
-    vector<int> nextGreaterElement(vector<int>& arr1, vector<int>& arr2) {
-        vector<int>next(arr2.size(), -1);
-        nge(arr2 , next);
-
-        vector<int>ans;
-
-        for(int i = 0 ; i < arr1.size() ; i++){
-            auto it = find(arr2.begin() , arr2.end() , arr1[i]);
-            if(it != arr2.end()){
-
-            auto index = distance(arr2.begin() , it);
-            auto ngeAtI = next[index];
-            ans.push_back(ngeAtI);
-            }
-            else{
-                ans.push_back(-1);
-            }
-
-        }
-
-        return ans;
+    if (st.empty()) {
+      ans[i] = -1;
+    } else {
+      ans[i] = st.top();
     }
+
+    st.push(nums2[i]);
+  }
+
+  vector<int> a(nums1.size());
+  for (int i = 0; i < nums1.size(); i++) {
+    int ele = nums1[i];
+    int ele_ind = -1;
+    for (int j = 0; j < nums2.size(); j++) {
+      if (ele == nums2[j]) {
+        ele_ind = j;
+        break;
+      }
+    }
+    a[i] = ans[ele_ind];
+  }
+  return a;
+}
 };
