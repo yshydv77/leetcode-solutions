ed5f152c61c78b775f96004653bb987bf145748f
yshydv77 <yshydv77@gmail.com>
2026-06-16 08:12:00 +0530

    Time: 0 ms (100%), Space: 14.9 MB (43.91%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -10,48 +10,44 @@
  */
 class Solution {
 public:
-    ListNode* partition(ListNode* head, int x) {
-        if(head == NULL || head->next == NULL){
-            return head;
-        }
 
-        ListNode* sHead = new ListNode (-1);
-        ListNode* gHead = new ListNode (-1);
+ListNode* partition(ListNode* head, int x) {
+  if(head == nullptr || head->next == nullptr){
+    return head;
+  }
 
-        ListNode* s = sHead;
-        ListNode* g = gHead;
-
-        ListNode* temp = head;
-
-        while(temp != NULL){
-            if(temp->val < x){
-                if(sHead->val == -1){sHead->val = temp->val;}
-                else{
-
-                    ListNode* newNode = new ListNode(temp->val);
-                    s->next = newNode;
-                    s = newNode;  
-                }
-            }
-            else if(temp->val >= x){
-                if(gHead->val == -1){gHead->val = temp->val;}
-                else{
-                    ListNode* newNode = new ListNode(temp->val);
-                    g->next = newNode;
-                    g = newNode;
-                }
-            }
-           
-            temp = temp->next;
-        }
-        if(sHead->val == -1){
-            return gHead;
-        }
-        else if(gHead->val == -1){
-            return sHead;
-        }
-        s->next = gHead;
-        return sHead;
-
+  ListNode* temp = head;
+  ListNode* less_x = nullptr;
+  ListNode* greater_x = nullptr;  // greater and equal to x
+  ListNode* h = nullptr;
+  ListNode* g = nullptr;
+  while (temp != nullptr) {
+    ListNode* node = new ListNode(temp->val);
+    if (temp->val < x) {
+      if (less_x == nullptr) {
+        less_x = node;
+        h = less_x;
+      } else {
+        less_x->next = node;
+        less_x = node;
+      }
+    } else if (temp->val >= x) {
+      if (greater_x == nullptr) {
+        greater_x = node;
+        g = greater_x;
+      } else {
+        greater_x->next = node;
+        greater_x = node;
+      }
     }
+    temp = temp->next;
+  }
+  if(h == nullptr){
+    return g;
+  }
+  else {
+    less_x->next = g;
+  }
+  return h;
+}
 };
