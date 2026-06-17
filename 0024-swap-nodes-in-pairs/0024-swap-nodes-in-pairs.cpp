9dae4b27ef0c7bfa0e07cf7f79b8b72a3def99da
yshydv77 <yshydv77@gmail.com>
2026-06-18 00:26:23 +0530

    Time: 0 ms (100%), Space: 11.1 MB (86.38%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+void reverseLL(ListNode* head , int k ){
+  // function to reverse the linked list k times
+  ListNode* curr = head;
+  ListNode* prev = nullptr;
+  while (k--)
+  {
+    ListNode* next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return ;
+}
+ListNode* swapPairs(ListNode* head) {
+  if (head == nullptr || head->next == nullptr) {
+    return head;
+  }
+
+  ListNode* prev_left = nullptr;
+  ListNode* left = head;
+  ListNode* res = nullptr;
+  int k = 2;
+
+  while (true) {
+    // mujhe pair kitne hai ye nahi pata hai isliye liye infinite while loop use kar rahe hai 
+    ListNode* right = left;
+    for (int i = 0; i < k - 1; i++) {
+      if (right == nullptr) {
+        break;
+      }
+      right = right->next;
+    }
+
+    if(right != nullptr){
+      ListNode* next_left = right->next;
+      reverseLL(left , k );
+      if(prev_left != nullptr){
+        prev_left->next = right;
+      }
+      prev_left = left;
+      if(res == nullptr){
+        res = right;
+      }
+
+      left = next_left;
+    } 
+    else{
+      if(prev_left != nullptr){
+        prev_left ->next = left;
+      }
+      if(res == nullptr){
+        res = left;
+      }
+      break;
+    }
+  }
+
+  return res;
+}
+};
