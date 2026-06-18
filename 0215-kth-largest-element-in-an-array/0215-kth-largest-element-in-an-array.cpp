b524a9568c199bd28ca31544077a4b57c898f3cf
yshydv77 <yshydv77@gmail.com>
2026-06-18 14:33:10 +0530

    Time: 20 ms (86.43%), Space: 72.3 MB (14.07%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
-// kth largest max heap question
-int findKthLargest(vector<int>& nums, int k) {
-  priority_queue<int>pq;
+  // kth largest max heap question
+  int findKthLargest(vector<int> & nums, int k) {
+ priority_queue<int,vector<int>, greater<int>>pq;
+    // max heap 
+    
+    for (int i = 0 ;i < k ;i++)
+    { 
+      pq.emplace(nums[i]);
+    }
+    for(int i = k ;i < nums.size() ; i++){
+      if(pq.top() < nums[i]){
+        pq.pop();
+        pq.push(nums[i]);
+      }
+      else if(pq.top() >= nums[i]){
+        continue;
+      }
+    }
+    
 
-  for (auto& it : nums) {
-    pq.push(it);
-  }
-  k--;
-  while (k--)
-  {
-    pq.pop();
-  }
-  return pq.top();
+    return pq.top();
+    
+    
+    
+    // priority_queue<int> pq;
 
-}
+    // for (auto& it : nums) {
+    //   pq.push(it);
+    // }
+    // k--;
+    // while (k--) {
+    //   pq.pop();
+    // }
+    // return pq.top();
+  }
 };
