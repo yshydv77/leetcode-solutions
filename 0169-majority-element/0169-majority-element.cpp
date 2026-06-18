fb85c3be3a9b3789b14d69ecc210563acf7abac2
yshydv77 <yshydv77@gmail.com>
2026-06-18 12:11:57 +0530

    Time: 0 ms (100%), Space: 28.1 MB (87.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int i = 0 ;i < nums.size() ; i++){
-            mp[nums[i]]++;
-        }
-        int majf = INT_MIN;
-        int maje = -1;
-        for(auto it  : mp){
-            if(majf < it.second){
-                majf = it.second;
-                maje = it.first;
-            }
-        }
-        return maje;
+int majorityElement(vector<int>& nums) {
+  int candidate = 0;
+  int counter = 0;
+  for (int i = 0; i < nums.size(); i++) {
+    if (counter == 0) {
+      // iska matlab mera new candidate aagaya hai
+      // yaa phir mera phele hi elemenet hia
+      candidate = nums[i];
+      counter = 1;
     }
+
+    else if ( candidate == nums[i]) {
+      counter++;
+    }
+    else if(candidate != nums[i]){
+      counter--;
+    }
+  }
+
+  return candidate;
+}
 };
