2861e39450d68fe4d9bff4a4cb03e35e6bdb2e22
yshydv77 <yshydv77@gmail.com>
2026-06-20 09:11:06 +0530

    Time: 0 ms (100%), Space: 36.3 MB (63.75%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,30 +1,56 @@
 class Solution {
 public:
-vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-  int n = arr.size();
-  // closest bola hai k closest hai toh 
-  // closest pucha haii toh max heap ka use karnege
-  priority_queue<pair<int,int>>pq;
-  for (int i = 0; i < n; i++)
+vector<int> findClosestElements(vector<int>& arr, int k,int x) {
+  int low = 0;
+  int high= arr.size()-1;
+  while (high-low+1 > k)
   {
-    if(pq.size() < k ){
-      pq.push({abs(x-arr[i]) , arr[i]});
+    int xLow = abs(x-arr[low]);
+    int xHigh = abs(x-arr[high]);
+
+    if(xLow < xHigh){
+      high--;
+    }
+    else if(xLow > xHigh){
+      low++;
     }
     else{
-      pq.push({abs(x-arr[i]) , arr[i]});
-      pq.pop();
+      high--;
     }
   }
   vector<int>ans;
-  while (!pq.empty())
-  {
-    ans.push_back(pq.top().second);
-    pq.pop();
+  for(int i = low ; i<= high ; i++){
+    ans.push_back(arr[i]);
   }
-  sort(ans.begin(),ans.end());
+
   return ans;
+  
+}
+// vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//   int n = arr.size();
+//   // closest bola hai k closest hai toh 
+//   // closest pucha haii toh max heap ka use karnege
+//   priority_queue<pair<int,int>>pq;
+//   for (int i = 0; i < n; i++)
+//   {
+//     if(pq.size() < k ){
+//       pq.push({abs(x-arr[i]) , arr[i]});
+//     }
+//     else{
+//       pq.push({abs(x-arr[i]) , arr[i]});
+//       pq.pop();
+//     }
+//   }
+//   vector<int>ans;
+//   while (!pq.empty())
+//   {
+//     ans.push_back(pq.top().second);
+//     pq.pop();
+//   }
+//   // sort(ans.begin(),ans.end());
+//   return ans;
   
   
 
-}
+// }
 };
