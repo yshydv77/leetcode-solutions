e30215fd420d91ee6abe087ade8f3bd5c320d234
yshydv77 <yshydv77@gmail.com>
2026-07-02 19:03:35 +0530

    Time: 14 ms (22.89%), Space: 7.9 MB (20.12%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0 || n == 1){return n;}
-        int fib1 = fib(n-1);
-        int fib2 = fib(n-2);
-        return fib1 + fib2 ;
+        if(n == 0 ||  n== 1){
+            return n;
+        }
+
+        return fib(n-1)+fib(n-2);
     }
 };
