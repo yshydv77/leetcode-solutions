6a8d8e8247e196719d9ea2e9a448b4428e514e44
yshydv77 <yshydv77@gmail.com>
2026-07-25 10:26:01 +0530

    Time: 0 ms (100%), Space: 12.4 MB (88.57%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void fun(TreeNode* root){
+  // agar null ke equal ho gaya root then iska matlab hai ki koi child present hi nahi hai 
+  // jisko mujhe swap karna hai 
+  if(root == nullptr){
+    return ;
+  }
+
+  swap(root->left , root->right);
+  fun(root->left);
+  fun(root->right);
+}
+TreeNode *invertTree(TreeNode *root)
+{
+  fun(root);
+  return root;
+}
+};
