2389597c14d6c0df08ed382f2066126cbeeac17b
yshydv77 <yshydv77@gmail.com>
2026-08-07 15:40:41 +0530

    Time: 27 ms (55.6%), Space: 16.5 MB (67.55%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+bool isValid(int row, int col, int totalRows, int totalCols)
+{
+  if (row < 0 || row >= totalRows || col < 0 || col >= totalCols)
+  {
+    return false;
+  }
+  return true;
+}
+void dfs(vector<vector<char>> &grid, int totalRows, int totalCols, int i, int j, vector<vector<bool>> &visited)
+{
+  visited[i][j] = true;
+
+  int x[4] = {-1, 1, 0, 0};
+  int y[4] = {0, 0, -1, 1};
+  for (int k = 0; k < 4; k++)
+  {
+    int row = i + x[k];
+    int col = j + y[k];
+    if (isValid(row, col, totalRows, totalCols) && grid[row][col] == '1' && visited[row][col] == false)
+    {
+      dfs(grid, totalRows, totalCols, row, col, visited);
+    }
+
+  }
+}
+int numIslands(vector<vector<char>> &grid)
+{
+  int totalRows = grid.size();
+  int totalCols = grid[0].size();
+  vector<vector<bool>> visited(totalRows, vector<bool>(totalCols, false));
+  int numOfIsland = 0;
+  for (int i = 0; i < totalRows; i++)
+  {
+    for (int j = 0; j < totalCols; j++)
+    {
+      if (grid[i][j] == '1' && visited[i][j] == false)
+      {
+        dfs(grid, totalRows, totalCols, i, j, visited);
+        numOfIsland++;
+      }
+    }
+  }
+  return numOfIsland;
+}
+
+};
