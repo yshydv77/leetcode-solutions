d987535e3a40db163fd61275c813cf08d54aca44
yshydv77 <yshydv77@gmail.com>
2026-08-22 21:01:05 +0530

    Time: 71 ms (54.02%), Space: 14.3 MB (61.81%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    int fun(vector<int>&nums , int n , vector<vector<int>>&dp , int i , int prev ){
-        // yaha par jab prev = -1 hoga tab dp mein -1 access karne par segmentation fault aajayega 
-        // usko handle karne ke liye values ko hum prev+1 mein store karenge but vo vaue prev vali value hogi 
-
-
-        if(i == n){
-            return 0; // because at the end we got nothing
-        }
-        if(dp[i][prev+1] != -1){
-            return dp[i][prev+1];
-        }
-        if(prev == -1 || nums[i] >nums[prev]){
-            // then we have two choices
-            // first -> include that
-            // second -> do not include that
-            int c1 = 1 + fun(nums, n , dp , i+1 , i);
-            int c2 = fun(nums,n,dp,i+1,prev);
-            return dp[i][prev+1] = max(c1,c2);
-        }
-
-        else{
-            // we do not have the choices 
-            return dp[i][prev+1] = fun(nums,n,dp,i+1,prev);
-        }
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        int prev = -1;
-        int i = 0;
-        vector<vector<int>>dp(n+1 , vector<int>(n+1,-1));
-        // because yaha par i and prev change hoga and both are ranged from 0 to n 
-        return fun(nums , n , dp ,i , prev );
+        vector<int>res(n);
+        for(int i = 0 ; i < n ; i++){
+            res[i] = 1;
+            for(int j = 0 ; j < i ; j++){
+                if(nums[j] < nums[i]){
+                    res[i] = max(res[i] , res[j]+1);
+                }
+            }
+        }
+
+        int ans = 1;
+        for(int i = 0 ; i< n ; i++){
+            ans = max(ans , res[i]);
+        }
+
+        return ans;
     }
 };
